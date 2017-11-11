5eca3f3b16a466a44f7eaf64f4793b1566c524d8
George Vyshnya <gvyshnya@gmail.com>
2017-11-11 20:10:12 +0200

    !I: Initial submission

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# reactJSNewsMiniSite
-News mini-site implemented in React JS (with mini-server implemented in node.js)
+# Overview
+This is a basic show-case project to a News feed mini-site in ReactJS (with mini-server for it implemented in node.js).
+
+# Running the News Mini-Site
+To try the mini-site on your local machine, you should act as follows
+
+- make sure all of the software platforms and packages are pre-installed and properly configured on your machine (see _Installation and Configuration_ section below)
+- download or clone this repository to a local folder of your choice
+- open a command-line prompt utility and navigate to the above-mentioned local folder
+- start the server backend via the command: _npm start_
+- go to http://localhost:4061/ in any browser
+
+Once you do it, you will be able to
+- see the initial list of three news publications in the feed
+- add new posts with news at your discretion
+
+# Installation and Configuration
+
+Below are the important pre-requisites you should complete on your machine before you can run the News Mini-Site there
+
+- Install **node.js** platform on your local machine (download the right package for your platform at https://nodejs.org/en/download/ as well as follow the respective installation instruction)
+- Install **express** package into your **node.js** platform as a global package via the command: _npm install express -g_
+
+# Known Issues and Limitation
+
+- as this project is a basic show-case, the functionality to persist (store) the new news posts has not been implemented - thus the new posts you add will not be serialized and displayed after you restart the server
---
+++ js/app.js
@@ -0,0 +1,202 @@
+'use strict';
+
+var my_news = [
+  {
+    author: "Eileen Yu",
+    text: "Singapore opens up access to citizen data",
+    bigText: "Singapore opens up access to citizen data to facilitate business transactions. Commercial businesses can now access citizen data, such as mailing address and passport numbers, stored in the national MyInfo database, in a move the Singapore government says is aimed at improving service efficiency."
+  },
+  {
+    author: "Larry Dignan",
+    text: "Equifax spends $87.5 million on data breach",
+    bigText: "Equifax spends $87.5 million on data breach, more expenses on deck. Equifax's third quarter earnings and revenue weren't all that bad considering its data breach debacle."
+  },
+  {
+    author: "Danny Palmer",
+    text: "BankBot Android malware sneaks into the Google Play Store",
+    bigText: "BankBot Android malware sneaks into the Google Play Store - for the third time. More embarrassment for Google, as bank-data stealing malware infiltrates official Android app marketplace once again."
+  }
+];
+
+window.ee = new EventEmitter();
+
+var Article = React.createClass({
+  propTypes: {
+    data: React.PropTypes.shape({
+      author: React.PropTypes.string.isRequired,
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired
+    })
+  },
+  getInitialState: function() {
+    return {
+      visible: false
+    };
+  },
+  readmoreClick: function(e) {
+    e.preventDefault();
+    this.setState({visible: true});
+  },
+  render: function() {
+    var author = this.props.data.author,
+        text = this.props.data.text,
+        bigText = this.props.data.bigText,
+        visible = this.state.visible;
+
+    return (
+      <div className='article'>
+        <p className='news__author'>{author}:</p>
+        <p className='news__text'>{text}</p>
+        <a href="#"
+          onClick={this.readmoreClick}
+          className={'news__readmore ' + (visible ? 'none': '')}>
+          More
+        </a>
+        <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+      </div>
+    )
+  }
+});
+
+var News = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+  getInitialState: function() {
+    return {
+      counter: 0
+    }
+  },
+  render: function() {
+    var data = this.props.data;
+    var newsTemplate;
+
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index) {
+        return (
+          <div key={index}>
+            <Article data={item} />
+          </div>
+        )
+      })
+    } else {
+      newsTemplate = <p>Unfortunately there are now news yet.</p>
+    }
+
+    return (
+      <div className='news'>
+        {newsTemplate}
+        <strong
+          className={'news__count ' + (data.length > 0 ? '':'none') }>Total news: {data.length}</strong>
+      </div>
+    );
+  }
+});
+
+var Add = React.createClass({
+  getInitialState: function() {
+    return {
+      agreeNotChecked: true,
+      authorIsEmpty: true,
+      textIsEmpty: true
+    };
+  },
+  componentDidMount: function() {
+    ReactDOM.findDOMNode(this.refs.author).focus();
+  },
+  onBtnClickHandler: function(e) {
+    e.preventDefault();
+    var textEl = ReactDOM.findDOMNode(this.refs.text);
+
+    var author = ReactDOM.findDOMNode(this.refs.author).value;
+    var text = textEl.value;
+
+    var item = [{
+      author: author,
+      text: text,
+      bigText: '...'
+    }];
+
+    window.ee.emit('News.add', item);
+
+    textEl.value = '';
+    this.setState({textIsEmpty: true});
+  },
+  onCheckRuleClick: function(e) {
+    this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+  },
+  onFieldChange: function(fieldName, e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({[''+fieldName]:false})
+    } else {
+      this.setState({[''+fieldName]:true})
+    }
+  },
+  render: function() {
+    var agreeNotChecked = this.state.agreeNotChecked,
+        authorIsEmpty = this.state.authorIsEmpty,
+        textIsEmpty = this.state.textIsEmpty;
+    return (
+      <form className='add cf'>
+        <input
+          type='text'
+          className='add__author'
+          onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+          placeholder='Your name'
+          ref='author'
+        />
+        <textarea
+          className='add__text'
+          onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+          placeholder='Text of the news post'
+          ref='text'
+        ></textarea>
+        <label className='add__checkrule'>
+          <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>I accept the terms and conditions
+        </label>
+
+        <button
+          className='add__btn'
+          onClick={this.onBtnClickHandler}
+          ref='alert_button'
+          disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
+          >
+          Publish the news
+        </button>
+      </form>
+    );
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      news: my_news
+    };
+  },
+  componentDidMount: function() {
+    var self = this;
+    window.ee.addListener('News.add', function(item) {
+      var nextNews = item.concat(self.state.news);
+      self.setState({news: nextNews});
+    });
+  },
+  componentWillUnmount: function() {
+    window.ee.removeListener('News.add');
+  },
+  render: function() {
+    console.log('render');
+    return (
+      <div className='app'>
+        <Add />
+        <h3>News</h3>
+        <News data={this.state.news} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
